d5301d235f7736201a4475b680120e7cb0ed0355
Saivardhan-123 <saivardhansaineni@gmail.com>
2026-06-26 12:42:21 +0530

    Deploy commit

--- app.py
+++ app.py
@@ -39,5 +39,12 @@ def predict():
         prediction="The person survived"
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
